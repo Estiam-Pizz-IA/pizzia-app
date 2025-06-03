e4f9b65473cceb036d7281c5e3a0601690a76edd
faraan-estiam <faraan.rozbully@estiam.com>
2025-06-03 23:14:54 +0200

    refactored auth using the middleware

--- pizzapi/routers/auth.js
+++ pizzapi/routers/auth.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { authRegister, authLogin, authProfile, authLogout } = require('../controllers/auth');
+const { requireAuth } = require('../middlewares/auth');
 const router = express.Router();
 
 router.post('/register', authRegister);
 router.post('/login', authLogin);
-router.post('/logout', authLogout);
-router.get('/profile', authProfile);
+router.post('/logout', requireAuth, authLogout);
+router.get('/profile', requireAuth, authProfile);
 
 module.exports = router;
