87fafb71dd52bf4f7bce4d6e9353c23684f35edc
Greglhub <76042882+Greglhub@users.noreply.github.com>
2025-06-03 20:16:43 +0200

    update connexion et pizza

--- cypress/e2e/connexion.cy.js
+++ cypress/e2e/connexion.cy.js
@@ -14,7 +14,8 @@ it('Connexion avec un utilisateur valide', () => {
     cy.get('input[placeholder="Mot de passe"]').type(mdp)
     cy.get('.page-module__X_Z_4a__btn_connexion').contains('Connexion').click({force:true})
     cy.wait(5000)
-    cy.get('.navbar-module__QXtwcq__nav_item').contains('ACCEUIL').should('be.visible')
+    cy.get('.navbar-module__QXtwcq__nav_item').contains('PROFILE').click({force:true}) 
+    cy.get('.page-module__h4SgrW__profil_item').contains('test3.cypress@gmail.com').should('be.visible')
 })
 
 it('Connexion avec un mauvais utilisateur et un bon mdp', () => {
--- cypress/e2e/pizza.cy.js
+++ cypress/e2e/pizza.cy.js
@@ -1,4 +1,7 @@
 import {UrlPizza} from "./../fixtures/pizza.json"
+import {pizza} from "./../fixtures/pizza.json"
+import {description} from "./../fixtures/pizza.json"
+import {prix} from "./../fixtures/pizza.json"
 
 beforeEach(() =>{
     cy.visit(UrlPizza)
@@ -6,17 +9,17 @@ beforeEach(() =>{
 
 it('Affichage des pizzas', ()=>{
  
-    cy.get(".cardProduct-module__B2l_pG__productCard").contains('Pizza Hawaïenne').should('be.visible')
+    cy.get(".cardProduct-module__B2l_pG__productCard").contains(pizza).should('be.visible')
 
 })
 
 
 it('Affichage de la composition des pizzas', ()=>{
  
-    cy.contains('desc').should('exist')
+    cy.get('.cardProduct-module__B2l_pG__productCard').contains(description).should('exist')
 
 })
 
 it('Affichage du prix des pizzas', ()=>{
-     cy.get(':nth-child(1) > :nth-child(3) > strong').should('have.text', '1 €')
+     cy.get(':nth-child(1) > :nth-child(3) > strong').contains(prix).should('be.visible')
 })
